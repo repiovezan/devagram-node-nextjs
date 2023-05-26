a7d52cae10d36c9c6ad0f81f6348c03b262e9b45
repiovezan <repiovezan@hotmail.com>
2023-05-26 06:49:32 -0300

    configuracoes>banco de dados

---
+++ pages/api/login.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest , NextApiResponse } from "next";
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
+
+const endpointLogin = (
+    req : NextApiRequest,
+    res : NextApiResponse<RespostaPadraoMsg>
+) => {
+    if(req.method === 'POST'){
+    const {login, senha} = req.body;
+
+        if(login === 'admin@admin.com' && 
+        senha === 'Admin@123'){
+            res.status(200).json({msg : 'Usuário autenticado com sucesso'});
+        }
+        return res.status(400).json({erro : 'Usuário ou senha não encontrado'});
+    }
+    return res.status(405).json({erro : 'Método informado não é válido'});
+}
+
+export default conectarMongoDB(endpointLogin);
