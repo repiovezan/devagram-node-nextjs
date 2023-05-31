68fc49c66f04ef09b18b37d3ee63b873a9938f27
repiovezan <repiovezan@hotmail.com>
2023-05-31 06:22:01 -0300

    conclusao config jwt

---
+++ pages/api/usuario.ts
@@ -0,0 +1,8 @@
+import type {NextApiRequest, NextApiResponse} from 'next'
+import { validarTokenJWT } from '@/middlewares/validarTokeJWT'
+
+const usuarioEndPoint = (req : NextApiRequest, res : NextApiResponse) => {
+    return res.status(200).json('Usuário autenticado com sucesso')
+}
+
+export default validarTokenJWT(usuarioEndPoint);
