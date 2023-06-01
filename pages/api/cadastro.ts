2e63929dae5383602c83d038f8ea2c3d4dfa183d
repiovezan <repiovezan@hotmail.com>
2023-06-01 07:38:43 -0300

    cadastro_avatar_erro2

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -10,6 +10,7 @@ import nc from 'next-connect';
 const handler = nc()
     .use(upload.single('file'))
     .post(async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+        console.log('cadastro endpoint' , req.body)
         try{
             const usuario = req.body as CadastroRequisicao;
         
@@ -47,7 +48,7 @@ const handler = nc()
             return res.status(200).json({msg : 'Usuario criado com sucesso'});
         }catch(e : any){
             console.log(e);
-            return res.status(400).json({erro : e.toString()});
+            return res.status(500).json({erro : 'Erro ao cadastrar usuario'});
         }
 });
 
--- services/uploadImagemCosmic.ts
+++ services/uploadImagemCosmic.ts
@@ -23,11 +23,14 @@ const storage = multer.memoryStorage();
 const upload = multer({storage : storage});
 
 const uploadImagemCosmic = async(req : any) => {
+    console.log('uploadrequisicaoimagem', req)
     if(req?.file?.originalname){
         const media_object = {
             originalname: req.file.originalname,
             buffer : req.file.buffer};
 
+        console.log('uploadrequisicaoimagem url', req.url)
+        console.log('uploadrequisicaoimagem media_object', media_object)
         if(req.url && req.url.includes('publicacao')){
             return await bucketPublicacoes.addMedia({media : media_object})
         }else{
