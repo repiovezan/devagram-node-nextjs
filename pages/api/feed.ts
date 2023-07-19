7395e124d591f3ba9f970dc100148c2f24ec3924
repiovezan <repiovezan@hotmail.com>
2023-07-19 15:22:27 -0300

    feed finalizado

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -4,6 +4,7 @@ import {validarTokenJWT} from '../../middlewares/validarTokeJWT';
 import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
 import { UsuarioModel } from '../../models/UsuarioModel';
 import { PublicacaoModel } from '../../models/PublicacaoModel';
+import { SeguidorModel } from '../../models/SeguidorModel';
 
 
 const feedEndpoint = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any>) => {
@@ -21,13 +22,46 @@ const feedEndpoint = async (req : NextApiRequest, res : NextApiResponse<Resposta
                 const publicacoes = await PublicacaoModel
                     .find({idUsuario : usuario._id})
                     .sort({data : -1});
-                    return res.status(200).json(publicacoes);
-            }}
-            
+
+                return res.status(200).json(publicacoes);
+            }else{
+                const {userId} = req.query;
+                const usuarioLogado = await UsuarioModel.findById(userId);
+                if(!usuarioLogado){
+                    return res.status(400).json({erro : 'Usuario nao encontrado'});
+                }
+
+                const seguidores = await SeguidorModel.find({usuarioId : usuarioLogado._id});
+                const seguidoresIds = seguidores.map(s => s.usuarioSeguidoId);
+
+                const publicacoes = await PublicacaoModel.find({
+                    $or : [
+                        {idUsuario : usuarioLogado._id},
+                        {idUsuario : seguidoresIds}
+                    ]
+                })
+                .sort({data : -1});
+
+                const result = [];
+                for (const publicacao of publicacoes) {
+                   const usuarioDaPublicacao = await UsuarioModel.findById(publicacao.idUsuario);
+                   if(usuarioDaPublicacao){
+                        const final = {...publicacao._doc, usuario : {
+                            nome : usuarioDaPublicacao.nome,
+                            avatar : usuarioDaPublicacao.avatar
+                        }};
+                        result.push(final);  
+                   }
+                }
+
+                return res.status(200).json(result);
+            }
+        }
+        return res.status(405).json({erro : 'Metodo informado nao e valido'});
     }catch(e){
         console.log(e);
-        return res.status(400).json({erro : 'Não foi possível carregar o feed.'})
     }
+    return res.status(400).json({erro : 'Nao foi possivel obter o feed'});
 }
 
-export default validarTokenJWT(conectarMongoDB(feedEndpoint));
+export default (validarTokenJWT(conectarMongoDB(feedEndpoint)));
